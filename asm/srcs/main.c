1287e1d129f6291886ac931293781bcdacc59147
marel_m <maud.marel@epitech.eu>
2016-03-17 16:35:29 +0100

    lexer ok, test avec champions tous ok

--- asm/srcs/main.c
+++ asm/srcs/main.c
@@ -5,7 +5,7 @@
 ** Login   <marel_m@epitech.net>
 **
 ** Started on  Fri Mar 11 13:57:07 2016
-** Last update Thu Mar 17 10:11:12 2016 
+** Last update Thu Mar 17 16:22:25 2016 
 */
 
 #include "asm.h"
@@ -14,16 +14,14 @@ int		main(int ac, char **av)
 {
   t_header	header;
   t_instruc	instruc;
-  int		i;
 
   if (ac == 1)
     {
       write(2, "Wrong arguments\n", 16);
       return (-1);
     }
-  i = 0;
-  while (++i < ac)
-    lexer(&header, &instruc, av[i]);
-  /* parser(av[1], &header, &instruc); */
+  lexer(&header, &instruc, av[1]);
+  print_list(&instruc);
+  parser(av[1], &header, &instruc);
   return (0);
 }
--- asm/toto.s
+++
@@ -1,7 +0,0 @@
-	.name "zork"
-	.comment "just a basic living prog"
-l2:
-	sti r12,%1,%42
-	and %12,%0,r1
-live:	zjmp %:live
-	zjmp %:live
