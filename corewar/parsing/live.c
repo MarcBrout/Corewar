d32039d560482e1c72db078d0f81df1b87453e63
duhieu_b <benjamin.duhieu@epitech.eu>
2016-03-24 12:30:10 +0000

    ok

--- corewar/parsing/live.c
+++ corewar/parsing/live.c
@@ -1,11 +1,11 @@
 /*
-** live-suv.c for live-sub in /home/duhieu_b/CPE/CPE_2015_corewar/vm/parsing
+** live-suv.c for live-sub in /CPE/CPE_2015_corewar/vm/parsing
 **
 ** Made by benjamin duhieu
 ** Login   <duhieu_b@epitech.net>
 **
 ** Started on  Mon Mar 21 21:40:56 2016 benjamin duhieu
-** Last update Thu Mar 24 11:44:56 2016 benjamin duhieu
+** Last update Thu Mar 24 12:28:43 2016 benjamin duhieu
 */
 
 #include "vm.h"
@@ -15,11 +15,14 @@ int	live(t_data *data, t_pc *i)
   int	chk;
   int	a;
 
-  chk = read_int_from_ram(data->ram, i->reg[0]);
+  chk = read_int_from_ram(data->ram, MM(i->reg[0] + 1));
   a = -1;
   while (++a < 4)
-    if (chk == data->champ[a]->pc->reg[1])
-      data->champ[a]->alive = 1;
+    {
+      printf("chk = %d, data->champ[a] : %d\n", chk, data->champ[a]->pc->reg[1]);
+      if (chk == data->champ[a]->pc->reg[1] && data->champ[a]->alive == 0)
+	data->champ[a]->alive = 1;
+    }
   i->cycle = 10;
   data->nblive++;
   i->reg[0] = MM(i->reg[0] + 5);
