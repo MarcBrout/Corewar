490b1350fe1789918de9ca7389f0232396616cd8
marel_m <maud.marel@epitech.eu>
2016-03-26 20:08:48 +0100

    bugg fixed asm

--- asm/srcs/lexer/check_comment.c
+++ asm/srcs/lexer/check_comment.c
@@ -5,7 +5,7 @@
 ** Login   <marel_m@epitech.net>
 **
 ** Started on  Thu Mar 10 17:20:59 2016
-** Last update Sat Mar 26 19:06:32 2016 marel_m
+** Last update Sat Mar 26 20:07:10 2016 marel_m
 */
 
 #include <stdlib.h>
@@ -42,15 +42,20 @@ void	stock_comment(t_header *header, char *file)
   while (++i != (my_strlen(file) - 1))
     header->comment[j++] = file[i];
   header->comment[j] = '\0';
+  printf("%s\n", header->comment);
 }
 
-void	no_comment(t_header *header)
+int	no_comment(t_instruc *instruc, t_header *header, char *new)
 {
-  stock_comment(header, "gg");
-  write(1, "warning: no comment specified\n", 30);
+  header->comment[0] = '\0';
+  write(2, "Warning: no comment specified.\n", 31);
+  if ((instruc->first_no_c = my_strdup(new)) == NULL)
+    return (-1);
+  return (0);
 }
 
-int	check_comment(t_header *header, t_instruc *instruc, char *file)
+int	check_comment(t_header *header, t_instruc *instruc,
+		      char *file)
 {
   char	*new;
 
@@ -61,7 +66,8 @@ int	check_comment(t_header *header, t_instruc *instruc, char *file)
     {
       if (my_strncmp(new, ".name", 5) == 0)
 	return (write(2, "Name can only be defined once.\n", 31), -1);
-      no_comment(header);
+      if (no_comment(instruc, header, new) == -1)
+	return (-1);
       return (free(new), 1);
     }
   free(new);
@@ -72,5 +78,24 @@ int	check_comment(t_header *header, t_instruc *instruc, char *file)
     return (synthax_error(instruc, 0), -1);
   stock_comment(header, new);
   free(new);
+  return (0);
+}
+
+int	put_instruc_no_comm(t_instruc *instruc, int fd)
+{
+  instruc->nb_l++;
+  if (instruc->first_no_c[0] == '#' || instruc->first_no_c[0] == ';'
+      || my_strlen(instruc->first_no_c) == 0
+      || check_line(instruc->first_no_c) == -1
+      || if_comment_text(instruc->first_no_c) == -1)
+    {
+      if (put_instruc(instruc, fd) == -1)
+	return (-1);
+    }
+  else if (check_instruc_arg(instruc, instruc->first_no_c, fd) == -1
+	   || put_instruc(instruc, fd) == -1)
+    return (-1);
+  free(instruc->first_no_c);
+  put_instruc(instruc, fd);
   return (0);
 }
